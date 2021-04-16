e5fc1c0660ea87df76ad54321c960cc1fe6eeb74
Vitor Giovane <vitorgpalves@gmail.com>
2021-04-15 23:09:32 -0300

    feat: implements static generation with external data

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,9 @@ import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-export default function Home() {
+import { getSortedPostsData } from '../lib/posts'
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -21,6 +23,22 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
+      </section>
+
       <br />
       <h2>
         <Link href='/posts/first-post'>
@@ -29,4 +47,14 @@ export default function Home() {
       </h2>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
